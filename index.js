95991298455b7948eaaa52f3f6934fd22d84524d
schmuhl2@outlook.com <schmuhl2@outlook.com>
2018-06-30 23:27:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 // write your code below!
+<<<<<<< HEAD
 function happyHolidays(string) {
   return "Happy holidays!"
 }
+=======
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
+}
+>>>>>>> 4b5416e3dfc264ee2393b3fcdd7529de77215a7d
