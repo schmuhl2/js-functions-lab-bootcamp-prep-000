d5e5107a84d3518a292099a14cf30204a8204fb6
schmuhl2@outlook.com <schmuhl2@outlook.com>
2018-06-30 23:28:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 function happyHolidays(string) {
   return "Happy holidays!"
 }
-=======
 function happyHolidays() {
   return "Happy holidays!"
 }
@@ -18,4 +17,4 @@ function happyHolidayTo(holiday, name) {
 function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
->>>>>>> 4b5416e3dfc264ee2393b3fcdd7529de77215a7d
+
